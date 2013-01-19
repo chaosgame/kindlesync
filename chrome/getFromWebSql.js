22337beedd29ebad530d9c8a5152e82501a8da51
Nathan Lawrence <ch@osga.me>
2013-01-19 14:52:06 -0800

    [chrome] Get the device token from WebSql if possible

---
+++ chrome/getFromWebSql.js
@@ -0,0 +1,22 @@
+(function(key, id) {
+    var db = openDatabase('K4W', '', 'Kindle Cloud Reader', 5e6);
+    db.transaction(function (tx) {
+        tx.executeSql(
+            "SELECT value FROM keyValue WHERE key = '" + key + "'",
+            [],
+            function (tx, results) {
+                var message = {
+                    'key' : key,
+                    'id' : id,
+                    'value' : null
+                };
+                if (results.rows.length == 1) {
+                    message['value'] = results.rows.item(0).value;
+                }
+                chrome.extension.sendMessage(
+                    message,
+                    function(r) { }
+                );
+            });
+        });
+})(document.URL.match(/key=(\w*)/)[1], document.URL.match(/id=(websql-[0-9]*)/)[1]);
--- chrome/kindlesync.js
+++ chrome/kindlesync.js
@@ -4,6 +4,8 @@
 
     // A javascript port of the python function
 
+    // TODO(nathan) need code to read from the local db in the client context...
+
     function urlencode(args) {
         return (_.map(
             args,
@@ -12,28 +14,50 @@
             })).join('&');
     }
 
+    function sync_with_server_login() {
+        /* $.ajax({
+            'url' : '127.0.0.1:5000/'
+        }); */
+    }
+
     function sync_with_sessionid(sessionid) {
         if (sessionid == null) {
             return;
         }
 
+        // X-Requested-With:XMLHttpRequest
         var headers = {
-            'x-amzn-sessionid' : sessionid.value
+            'x-amzn-sessionid' : sessionid.value,
+            'X-Requested-With' : 'XMLHttpRequest',
+            'Referrer' : 'http://read.amazon.com',
+            'Pragma' : 'no-cache',
+            'Cache-Control' : 'no-cache'
         };
 
-        $.ajax({
-            'url' : ('https://read.amazon.com/service/web/register/getDeviceToken?' +
-                urlencode({
-                    'serialNumber' : 'A2CTZ977SKFQZY',
-                    'deviceType' : 'A2CTZ977SKFQZY'
-                })),
-            'headers' : headers,
-            'async' : false,
-            'dataType' : 'json',
-            'success' : function(data) {
-                headers['X-ADP-Session-Token'] = data['deviceSessionToken'];
+        get_from_websql('kindleDeviceToken', function(deviceToken) {
+            if (deviceToken) {
+                headers['X-ADP-Session-Token'] = deviceToken;
+                sync_with_headers(headers);
             }
         });
+    }
+
+    function sync_with_headers(headers) {
+        if (!('X-ADP-Session-Token' in headers)) {
+            $.ajax({
+                'url' : ('https://read.amazon.com/service/web/register/getDeviceToken?' +
+                    urlencode({
+                        'serialNumber' : 'A2CTZ977SKFQZY',
+                        'deviceType' : 'A2CTZ977SKFQZY'
+                    })),
+                'headers' : headers,
+                'async' : false,
+                'dataType' : 'json',
+                'success' : function(data) {
+                    headers['X-ADP-Session-Token'] = data['deviceSessionToken'];
+                }
+            });
+        }
 
         var books;
         $.ajax({
@@ -54,12 +78,26 @@
     function sync_book_with_fragment_map(headers, book, data) {
         data = $.parseJSON(data.match(/{.*}/)[0]);
         var fragments = data['fragmentArray'];
+        // TODO(nathan) sometimes cPos is undefined
+        var last_position = fragments[fragments.length]['cPos'];
 
+        /* var url;
+        if (last_position >= ) {
+            url = '127.0.0.1:5000/update/' + book.asin + '/done/';
+        } else {
+            url = '127.0.0.1:5000/update/' + book.asin + '/progress/' + percent;
+        }
+
+        $.ajax({
+            'url' : url,
+        }); */
     }
 
     function sync_book_with_userdata(headers, book, data) {
         book.fragments_url = data['fragmentMapUrl'];
         book.last_page_read = data['lastPageReadData'];
+
+        console.log(book.last_page_read);
 
         $.ajax({
             'url' : book.fragments_url,
@@ -97,11 +135,36 @@
         }, sync_with_sessionid);
     }
 
+    websql_id = 1;
+    function get_from_websql(key, callback) {
+        $('body').append($('<iframe/>', {
+                'src' :
+                    'https://read.amazon.com/static/app/getFromWebSql.html?' +
+                    urlencode({ 'key' : key, 'id' : "websql-" + websql_id }),
+                'id' : "websql-" + websql_id
+            }).data('callback', callback));
+        websql_id++;
+    }
+
+    function get_from_websql_callback(request, sender, sendResponse) {
+        if (sender.tab.url ==
+                chrome.extension.getURL("_generated_background_page.html")) {
+            $iframe = $('#' + request.id);
+            callback = $iframe.data('callback');
+            $iframe.unload();
+            callback(request.value);
+        }
+        sendResponse({});
+    }
+
+    chrome.extension.onMessage.addListener(get_from_websql_callback);
+
     chrome.alarms.onAlarm.addListener(sync);
     chrome.alarms.create('sync', {
         'periodInMinutes' : 60,
         'when' : Date.now()
     });
+
     console.log('Loaded.');
 })();
 
